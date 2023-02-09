9ea00a151e12a613e3b2d1765f2d34b451706ac4
Jesper Josefsson <jesper.josefsson@paf.com>
2023-02-09 20:44:52 +0200

    Add succeeding test

--- src/test/java/com/paf/hey/HelloControllerTest.java
+++ src/test/java/com/paf/hey/HelloControllerTest.java
@@ -10,11 +10,20 @@ import io.quarkus.test.junit.QuarkusTest;
 @QuarkusTest
 class HelloControllerTest {
     @Test
-    void testHello() {
+    void testWithSeparator() {
         given().when()
                 .get("/hellos?names=foo,bar,baz")
                 .then()
                     .statusCode(200)
                     .body(is("Hello foo|bar|baz"));
+    }
+
+    @Test
+    void testWithMultipleParams() {
+        given().when()
+                .get("/hellos?names=foo&names=bar&names=baz")
+                .then()
+                .statusCode(200)
+                .body(is("Hello foo|bar|baz"));
     }
 }
